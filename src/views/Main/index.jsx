454d830a6f3f5c3977f19ef22043c0d92ab08ab3
Dheyson Alves <dheyson.lucas@hotmail.com>
2020-02-20 06:52:01 -0300

    Ajustes na calculadora

--- src/views/Main/index.jsx
+++ src/views/Main/index.jsx
@@ -4,43 +4,79 @@ import './styles.css';
 import Button from '../Button/index';
 import Display from '../Display/index';
 
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0
+}
+
 export default class Main extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {...initialState};
+    this.clearMemory = this.clearMemory.bind(this);
+    this.setOperation = this.setOperation.bind(this);
+    this.addDigit = this.addDigit.bind(this);
+  }
+
   clearMemory() {
-    console.log('limpar');
+    this.setState({...initialState});
   }
 
   setOperation(operation) {
-    console.log(operation);
+    if (this.state.current === 0) {
+      this.setState({operation, current: 1, clearDisplay: true})
+    } else {
+      const boolOperation = operation === '=';
+    }
   }
 
-  addDigit(n) {
-    console.log(n);
+  addDigit(value) {
+    if (value === '.' && this.state.displayValue.includes(value)) {
+      return;
+    }
+    const clearDisplay = this.state.displayValue === '0'|| this.state.clearDisplay;
+    const currentValue = clearDisplay ? '' : this.state.displayValue;
+    const displayValue = currentValue + value;
+
+    this.setState({displayValue, clearDisplay: false});
+    this.setValeOnArray(value, displayValue);
+  }
+
+  setValeOnArray (value, displayValue) {
+    if (value !== '.') {
+      const index = this.state.current;
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+      values[index] = newValue;
+      this.setState({values});
+    }
   }
 
   render() {
-    const addDigit = n => this.addDigit(n);
-    const setOperation = op => this.setOperation(op);
     return (
       <div className="calculator">
-        <Display value={100}/>
-        <Button txt="AC" click={() => this.clearMemory()} triple/>
-        <Button txt="/" click={() => this.setOperation('/')} operation/>
-        <Button txt="7" click={() => this.addDigit()}/>
-        <Button txt="8" click={() => this.addDigit('8')}/>
-        <Button txt="9" click={() => this.addDigit('9')}/>
-        <Button txt="*" operation click={() => this.setOperation('/')}/>
-        <Button txt="4" click={() => this.addDigit('4')}/>
-        <Button txt="5" click={() => this.addDigit()}/>
-        <Button txt="6" click={() => this.addDigit('7')}/>
-        <Button txt="-" operation click={() => this.setOperation('/')}/>
-        <Button txt="1" click={() => this.addDigit('7')}/>
-        <Button txt="2" click={() => this.addDigit('7')}/>
-        <Button txt="3" click={() => this.addDigit('7')}/>
-        <Button txt="+" operation click={() => this.setOperation('/')}/>
-        <Button txt="0" double click={() => this.addDigit('7')}/>
-        <Button txt="." click={() => this.setOperation('/')}/>
-        <Button txt="=" operation click={() => this.setOperation('/')}/>
+        <Display value={this.state.displayValue}/>
+        <Button txt="AC" click={this.clearMemory} triple/>
+        <Button txt="/" click={this.setOperation} operation/>
+        <Button txt="7" click={this.addDigit}/>
+        <Button txt="8" click={this.addDigit}/>
+        <Button txt="9" click={this.addDigit}/>
+        <Button txt="*" operation click={this.setOperation}/>
+        <Button txt="4" click={this.addDigit}/>
+        <Button txt="5" click={this.addDigit}/>
+        <Button txt="6" click={this.addDigit}/>
+        <Button txt="-" operation click={this.setOperation}/>
+        <Button txt="1" click={this.addDigit}/>
+        <Button txt="2" click={this.addDigit}/>
+        <Button txt="3" click={this.addDigit}/>
+        <Button txt="+" operation click={this.setOperation}/>
+        <Button txt="0" double click={this.addDigit}/>
+        <Button txt="." click={this.addDigit}/>
+        <Button txt="=" operation click={this.setOperation}/>
       </div>)
   }
 }
